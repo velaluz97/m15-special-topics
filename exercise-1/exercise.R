074f60003e8e192e041b10ee8a4fc41244a4bc6b
Lucy <luzv2@uw.edu>
2016-11-29 14:34:33 -0800

    Message

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -8,22 +8,50 @@
 library(gapminder)
 library(modelr)
 library(tidyverse)
+library(dplyr)
 
 # Initial view of the data with ggplot
+gapminder %>% 
+  ggplot(aes(year, lifeExp, group = country)) +
+  geom_line(alpha = 1/3)
 
 
 # Look only at new zealand
+nz <- filter(gapminder, country == "New Zealand")
+nz %>% 
+  ggplot(aes(year, lifeExp)) + 
+  geom_line() + 
+  ggtitle("Full data = ")
+
+nz_mod <- lm(lifeExp ~ year, data = nz)
+nz %>% 
+  add_predictions(nz_mod) %>%
+  ggplot(aes(year, pred)) + 
+  geom_line() + 
+  ggtitle("Linear trend + ")
+
+nz %>% 
+  add_residuals(nz_mod) %>% 
+  ggplot(aes(year, resid)) + 
+  geom_hline(yintercept = 0, colour = "white", size = 3) + 
+  geom_line() + 
+  ggtitle("Remaining pattern")
 
 
 
 # Better yet, write your own function to accept a country as a parameter,
 # and produce the same graphics
 
-# Nest the data by country/continent
 
+
+# Nest the data by country/continent
+by_country <- gapminder %>% 
+  group_by(country, continent) %>% 
+  nest()
 
 
 # Define a statistical model, and store it in a function
+
 
 
 # Use the `map` functionality to run the same model for each country separately
